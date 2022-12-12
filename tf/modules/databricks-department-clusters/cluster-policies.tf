c2b68dac7b1be481fb7b7bb0932c69f9cfa10b35
Vaibhav Sethi <78504446+vaibhavsethi-db@users.noreply.github.com>
2022-12-12 15:46:06 -0800

    reorganized content

---
+++ tf/modules/databricks-department-clusters/cluster-policies.tf
@@ -0,0 +1,35 @@
+resource "databricks_cluster_policy" "fair_use" {
+  name = "${var.department} Cluster Policy"
+  definition = jsonencode({
+    "dbus_per_hour" : {
+      "type" : "range",
+      "maxValue" : 10
+    },
+    "instance_pool_id" : {
+      "type" : "forbidden",
+      "hidden" : true
+    },
+    "driver_node_type_id" : {
+      "type" : "fixed",
+      "value" : data.databricks_node_type.smallest.id,
+      "hidden" : true
+    },
+    "autoscale.min_workers" : {
+      "type" : "fixed",
+      "value" : 1,
+      "hidden" : true
+    },
+    "custom_tags.Team" : {
+      "type" : "fixed",
+      "value" : var.department
+    }
+  })
+}
+
+resource "databricks_permissions" "can_use_cluster_policy" {
+  cluster_policy_id = databricks_cluster_policy.fair_use.id
+  access_control {
+    group_name       = databricks_group.data_eng.display_name
+    permission_level = "CAN_USE"
+  }
+}
